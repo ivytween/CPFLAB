f723fc3c396ecb054b752752f3deb8de6a7556c3
Chelsie Ivy Dela Cruz <dchelsieivy@gmail.com>
2021-10-15 14:32:08 +0800

    commit

---
+++ oe4.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+    int i,v,y,rows;
+    cout << "Number of Rows : ";
+    cin >> rows; 
+
+    for (i=1; i<=rows; i++)
+    {
+        for (y=i; y<rows; y++)
+        cout << " ";
+        for (v=1; v<=(2*rows-1); v++)
+        {
+            if (v==1 || v==2*i-1 || i==rows)
+            {
+                cout << "*";
+            }
+            else 
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+        for (i=1; i<=rows; i++)
+    {
+        for (y=i; y<rows; y++)
+        cout << " ";
+        for (v=1; v<=(2*rows-1); v++)
+        {
+            if (v==1 || v==2*i-1 || i==rows)
+            {
+                cout << "*";
+            }
+            else 
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+        for (i=1; i<=rows; i++)
+    {
+        for (y=i; y<rows; y++)
+        cout << " ";
+        for (v=1; v<=(2*rows-1); v++)
+        {
+            if (v==1 || v==2*i-1 || i==rows)
+            {
+                cout << "*";
+            }
+            else 
+            {
+                cout << " ";
+            }
+        }
+        cout << endl    QQ                                                                                          ;
+    }
+
+    return 0;
+}
